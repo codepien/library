e0151f311a026559165fa54e570dfe39ae50b541
Clint Oka <okaclint@gmail.com>
2021-09-16 17:45:54 -1000

    add book functionality

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
+let library = [];
+let displayLibrary = [];
 
+let inputTitle = document.querySelector("#inputTitle");
 
-function Book(name, author, pages) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
+let inputAuthor = document.querySelector("#inputAuthor");
+
+let inputPages = document.querySelector("#inputPages");
+
+function Book(_title, _author, _pages) {
+    this.title = _title
+    this.author = _author
+    this.pages = _pages
     this.info = function() {
-        return (this.name + " was written by " + this.author + " and has " + this.pages + " pages.")
+        return (this.title + " is written by " + this.author + " and has " + this.pages + " pages.")
     }
 }
 
-let HarryPotter = new Book("Harry Potter", "J.K. Rowling", 223);
- 
-console.log(HarryPotter.info());
+ function addToLibrary(_title, _author, _pages) {
+     let newBook = new Book(_title, _author, _pages);
+     library.push(newBook);
+ }
+
+let addButton = document.querySelector("#addButton");
+addButton.onclick = function(e) {
+    e.preventDefault();
+    addToLibrary(inputTitle.value, inputAuthor.value, inputPages.value);
+
+    for(let i = 0; i < displayLibrary.length; i++) {
+        let list = document.querySelector("#bookList");
+        let clearItem = list.getElementsByTagName("li");
+        list.removeChild(clearItem[0]);
+    }
+    
+    displayLibrary.length = 0;
+
+
+    for(i=0; i< library.length; i++) {
+        displayLibrary.push(library[i]);
+    }
+
+    
+
+    for(i=0; i< displayLibrary.length; i++) {
+        let listItem = document.createElement("li");
+        let book = displayLibrary[i];
+        let txt = document.createTextNode(book.info());
+        listItem.appendChild(txt);
+        let list = document.querySelector("#bookList");
+        list.appendChild(listItem);
+    }
+}
+
